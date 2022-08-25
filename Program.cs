af8a7b6e27605a646a6120f649e05afcb7caa6e2
Abhimishra77 <abhinavkumarmishra77@gmail.com.com>
2022-08-25 12:38:33 +0530

    day8newudate

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReadWriteIni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+             IniFile settingsIni = new IniFile (@".\\Testdata.ini");
+ 
+            string executionMode = settingsIni.Read ("BrowserName", "BrowserInfo").ToLower ();
+            Console.WriteLine (executionMode);
+ 
+            string WebdriverToUse = settingsIni.Read ("WebDriverLocation", "BrowserInfo").ToLower ();
+            Console.WriteLine (WebdriverToUse);
+            settingsIni.Write("TimeOut","4000","BrowserInfo");
+
+            string WebdriverTotime = settingsIni.Read ("TimeOut", "BrowserInfo");
+            Console.WriteLine (WebdriverTotime);
+            settingsIni.Write("BrowserName","IE","BrowserInfo");
+            settingsIni.Write("Url","www.amazon.in","AppUnderTest");
+            settingsIni.Write("Browser","CH","AppUnderTest");
+            settingsIni.Write("Timeout","6000","AppUnderTest");
+
+
+            
+
+        }
+    }
+}
